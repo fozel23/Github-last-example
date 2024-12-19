b47f2a8bf96754dc4c3db89c787b45c98f093293
fozel23 <fgoa23@gmail.com>
2024-12-19 19:12:59 +0300

    from local to remote desk

---
+++ src/AnaClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AnaClass {
+    public static void main(String[] args) {
+
+        Scanner oku= new Scanner(System.in);
+        String st1,st2;
+        st1= oku.nextLine();
+        st2= oku.next();
+        String st3=st1+st2;
+        System.out.println("ekrandan okunan " +st1);
+        System.out.println("okunan2"  +st2);
+        System.out.println("st3= "  +st3);
+
+
+    }
+}
